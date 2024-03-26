373b447e1177aa527e34065b95496f8e0ecb6b2a
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-03-26 17:18:40 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -0,0 +1,17 @@
+def is_group_word(word):
+    checked = [False for i in range(26)]
+    for i in range(len(word)):
+        chr = ord(word[i]) - ord("a")
+        if not checked[chr]:
+            checked[chr] = True
+        elif checked[chr] and word[i - 1] != word[i]:
+                return False
+    return True
+
+t = int(input())
+
+answer = 0
+for i in range(t):
+    if is_group_word(input()):
+        answer += 1
+print(answer)
