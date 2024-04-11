94f1ed2dd533eb913470387002cee772605630ef
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-11 20:05:24 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 8.21 ms, Memory: 28.5 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/README.md
@@ -1,10 +1,10 @@
 # [level 3] 연속 펄스 부분 수열의 합 - 161988 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161988?language=cpp) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 954.75 ms
+메모리: 28.5 MB, 시간: 8.21 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 20:00:42
+2024년 04월 11일 20:05:22
 
 ### 문제 설명
 
--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-long long max_sum(vector<int>& arr){
-    vector<long long> now_max(arr.size(), 0ll);
-    now_max[0] = arr[0];
-    long long result = arr[0];
+long long max_sum(vector<int>& seq){
+    vector<long long> now_max(seq.size(), 0ll);
+    now_max[0] = seq[0];
+    long long result = seq[0];
     
-    for(int i = 1; i < arr.size(); i++){
-        now_max[i] = max(0ll, now_max[i - 1]) + arr[i];
+    for(int i = 1; i < seq.size(); i++){
+        now_max[i] = max(0ll, now_max[i - 1]) + seq[i];
         result = max(result, now_max[i]);
     }
     
@@ -16,11 +16,11 @@ long long max_sum(vector<int>& arr){
 }
 
 long long solution(vector<int> sequence) {
-    vector<int> arr1(sequence);
-    for(int i = 0; i < arr1.size(); i+= 2) arr1[i] *= -1;
+    vector<int> seq1(sequence);
+    for(int i = 0; i < seq1.size(); i+= 2) seq1[i] *= -1;
     
-    vector<int> arr2(sequence);
-    for(int i = 1; i < arr2.size(); i+= 2) arr2[i] *= -1;
+    vector<int> seq2(sequence);
+    for(int i = 1; i < seq2.size(); i+= 2) seq2[i] *= -1;
     
-    return max(max_sum(arr1), max_sum(arr2));
+    return max(max_sum(seq1), max_sum(seq2));
 }
