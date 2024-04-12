cfc2c70bc2b83f4aae1017c9606c113736c2c060
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-12 22:13:29 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 4.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 11일 21:41:04
+2024년 04월 12일 22:13:26
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int get_parent(int node, vector<int>& parent){
+    if(parent[node] == node) return node;
+    return get_parent(parent[node], parent);
+}
+
+int solution(int n, vector<vector<int>> costs) {
+    vector<int> parent(n, 0);
+    for(int i = 0; i < n; i++) parent[i] = i;
+    
+    int answer = 0;
+    int cnt = 0;
+    sort(costs.begin(), costs.end(), [](auto a, auto b){ return a[2] < b[2]; });
+    for(vector<int> cost : costs){
+        int p1 = get_parent(cost[0], parent);
+        int p2 = get_parent(cost[1], parent);
+        
+        if(p1 != p2){
+            cnt++;
+            answer += cost[2];
+            parent[p2] = p1;
+            
+            if(cnt == n - 1) break;
+        }
+    }
+    
+    return answer;
+}
