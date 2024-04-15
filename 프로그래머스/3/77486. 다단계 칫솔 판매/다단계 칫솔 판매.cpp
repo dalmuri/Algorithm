e438106db4abb2dd435c8a24b647448a85177a4c
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-15 17:43:15 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 2062.38 ms, Memory: 18.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+void allocate(int seller_idx, int profit, vector<int>& parents, vector<int>& answer){
+    int to_give = profit / 10;
+    int mine = profit - to_give;
+    
+    answer[seller_idx] += mine;
+    if(parents[seller_idx] != -1) allocate(parents[seller_idx], to_give, parents, answer);
+}
+
+vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
+    map<string, int> idxs;
+    vector<int> parents(enroll.size(), -1);
+    for(int i = 0; i < enroll.size(); i++){
+        idxs[enroll[i]] = i;
+        
+        if(referral[i] != "-") parents[i] = idxs[referral[i]];
+    }
+    
+    vector<int> answer(enroll.size(), 0);
+    for(int i = 0; i < seller.size(); i++) allocate(idxs[seller[i]], amount[i] * 100, parents, answer);
+    
+    return answer;
+}
