95ac85b48db485e0900bb095f400ca8716e89e35
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-16 15:49:03 +0900

    [level 3] Title: 억억단을 외우자, Time: 268.36 ms, Memory: 47.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/138475. 억억단을 외우자/억억단을 외우자.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int e, vector<int> starts) {
+    vector<int> factors(e + 1, 1);
+    for(int i = 2; i <= e; i++){
+        int num = i;
+        while(num <= e){
+            factors[num]++;
+            num += i;
+        }
+    }
+    
+    vector<int> dp(e + 1, e);
+    for(int i = e - 1; i >= 1; i--){
+        if(factors[dp[i + 1]] > factors[i])
+            dp[i] = dp[i + 1];
+        else
+            dp[i] = i;
+    }
+    
+    vector<int> answer;
+    for(int start : starts) answer.push_back(dp[start]);
+    
+    return answer;
+}
