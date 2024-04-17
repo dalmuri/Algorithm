92150b43fb0aa467f0f1f4fc58da21430dfac5b0
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-17 22:38:45 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 66.78 ms, Memory: 17.6 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/README.md
@@ -1,10 +1,10 @@
 # [level 3] [카카오 인턴] 보석 쇼핑 - 67258 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67258?language=python3) 
 
 ### 성능 요약
 
-메모리: 12.9 MB, 시간: 80.47 ms
+메모리: 17.6 MB, 시간: 66.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 22:28:48
+2024년 04월 17일 22:38:44
 
 ### 문제 설명
 
---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,40 @@
+def solution(gems):
+    answer = [1, len(gems)]
+    
+    buy = {}
+    for gem in gems:
+        buy[gem] = 0
+    kinds = len(buy)
+    
+    start = 0
+    end = -1
+    num_kind = 0
+    while end + 1 < len(gems): # end가 gems의 마지막에 도착하기 전까지 반복
+        if num_kind < kinds:
+            end += 1
+            buy[gems[end]] += 1
+            if buy[gems[end]] == 1:
+                num_kind += 1
+        else:
+            buy[gems[start]] -= 1
+            if buy[gems[start]] == 0:
+                num_kind -= 1
+            start += 1
+        
+        if num_kind == kinds:
+            if end - start < answer[1] - answer[0]:
+                answer[0] = start + 1
+                answer[1] = end + 1
+    
+    while num_kind == kinds: # start를 1씩 증가하며 확인
+        buy[gems[start]] -= 1
+        if buy[gems[start]] == 0:
+            num_kind -= 1
+        start += 1
+        
+        if num_kind == kinds:
+            if end - start < answer[1] - answer[0]:
+                answer[0] = start + 1
+                answer[1] = end + 1
+    
+    return answer
