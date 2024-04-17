1ec33d0d3949b1d76c595bd20e892025c831d10a
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-17 23:32:45 +0900

    [level 3] Title: 기지국 설치, Time: 0.10 ms, Memory: 3.84 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/README.md
+++ 프로그래머스/3/12979. 기지국 설치/README.md
@@ -1,10 +1,10 @@
 # [level 3] 기지국 설치 - 12979 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12979?language=cpp) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 4.26 ms
+메모리: 3.84 MB, 시간: 0.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 17일 23:32:03
+2024년 04월 17일 23:32:44
 
 ### 문제 설명
 
--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -5,13 +5,13 @@ int solution(int n, vector<int> stations, int w)
 {
     int answer = 0;
     
+    int wave_len = w * 2 + 1; // 전파 전달 거리
     for(int i = 0; i <= stations.size(); i++){
         // 전파가 닿지 않는 아파트의 시작과 끝
         int start = (i == 0) ? 1 : stations[i - 1] + w + 1;
         int end = (i == stations.size()) ? n : stations[i] - w - 1;
         
         int count = end - start + 1; // 전파가 닿지 않는 아파트의 수
-        int wave_len = w * 2 + 1; // 전파 전달 거리
         if(count > 0) answer += count / wave_len + min(count % wave_len, 1);
     }
 
