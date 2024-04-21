607f0d49d149df648ff48b0a5332b5723adc0446
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-21 21:08:35 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,43 +1,21 @@
 def solution(n, m, x, y, r, c, k):
-    move = [0, 0, 0, 0] # down, left, right, up
-    word = ["d", "l", "r", "u"]
-    dir = [[1, 0], [0, -1], [0, 1], [-1, 0]]
-    cnt = k
-    if r - x > 0:
-        move[0] = r - x
-        cnt -= r - x
-    else:
-        move[3] = x - r
-        cnt -= x - r
+    down = 0
+    left = 0
+    right = 0
+    up = 0
     
-    if c - y > 0:
-        move[2] = c - y
-        cnt -= c - y
-    else:
-        move[1] = y - c
-        cnt -= y - c
+    if r - x > 0: down = r - x
+    else: up = x - r
     
-    if cnt < 0 or (cnt & 1) == 1: return "impossible"
+    if c - y > 0: right = c - y
+    else: left = y - c
     
-    answer = ""
+    k -= down + left + right + up
+    if k < 0 or (k & 1) == 1: return "impossible"
     
-    while k > 0:
-        for i in range(4):
-            if move[i] > 0:
-                answer += word[i]
-                k -= 1
-                x += dir[i][0]
-                y += dir[i][1]
-                move[i] -= 1
-                break
-            
-            if move[i] == 0 and cnt > 0 and 0 < x + dir[i][0] <= n and 0 < y + dir[i][1] <= m:
-                answer += word[i]
-                k -= 1
-                x += dir[i][0]
-                y += dir[i][1]
-                move[3 - i] += 1
-                cnt -= 2
-                break
+    down_over = min(n - max(r, x), k // 2)
+    k -= down_over * 2
+    left_over = min(min(c, y) - 1, k // 2)
+    k -= left_over * 2
     
-    return answer
+    return "d"*(down + down_over) + "l"*(left + left_over) + "rl"*(k // 2) + "r"*(right + left_over) + "u"*(up + down_over)
