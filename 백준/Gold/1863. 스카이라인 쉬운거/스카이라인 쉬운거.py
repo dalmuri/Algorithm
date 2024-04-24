d027841ec1bb11e1b3f06b6418eb03cb78eb065b
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-24 21:35:40 +0900

    [Gold IV] Title: 스카이라인 쉬운거, Time: 52 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
+++ 백준/Gold/1863. 스카이라인 쉬운거/스카이라인 쉬운거.py
@@ -1,17 +1,10 @@
 import sys
 
-def init_h(low, has_height):
-    while has_height[-1] > low:
-        has_height.pop()
-
-
 n = int(sys.stdin.readline())
 
 skyline = []
-max_h = 0
 for i in range(n):
     skyline.append(list(map(int, sys.stdin.readline().split())))
-    max_h = max(max_h, skyline[i][1])
 
 has_height = [0] # stack
 
