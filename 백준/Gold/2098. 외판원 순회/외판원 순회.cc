7c1c1300519e79391ce06975c8dfed7b5b45adce
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-27 20:32:43 +0900

    [Gold I] Title: 외판원 순회, Time: 24 ms, Memory: 6116 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.cc
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.cc
@@ -1,48 +1,44 @@
 #include <iostream>
 #include <cstring>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define INF 987654321
+
 using namespace std;
 
-#define INF 987654321;
+int n;
+int cost[16][16];
+int info[16][1<<16];
 
-int n,map[16][16];
-int dp[16][1<<16]; //각 마을이 방문한 도시를 2진법으로 저장
-
-int dfs(int cur, int visit){
+int search(int city, int route){
+    if(route == (1 << n) - 1){
+        if(cost[city][0] == 0) return INF;
+        return cost[city][0];
+    }
+    if(info[city][route] != -1) return info[city][route];
     
-    if (visit == (1<<n)-1){ //탐색 완료
-        if(map[cur][0] == 0) //이동불가능
-            return INF;
-        return map[cur][0];
+    info[city][route] = INF;
+    for(int i = 0; i < n; i++){
+        if(((1 << i) & route) != 0 || cost[city][i] == 0) continue;
+        info[city][route] = min(info[city][route], search(i, route | (1 << i)) + cost[city][i]);
     }
     
-    if (dp[cur][visit] != -1) //이미 탐색했으면
-        return dp[cur][visit];
-    
-    dp[cur][visit] = INF;
-    
-    for (int i=0; i<n; i++){
-        if (map[cur][i]==0) //길 X
-            continue;
-        if ((visit & (1<<i)) == (1<<i)) //이미 방문
-            continue;
-        dp[cur][visit] = min(dp[cur][visit], map[cur][i] + dfs(i, visit | 1<<i));
-    }
-    
-    return dp[cur][visit];
+    return info[city][route];
 }
 
-int main(int argc, const char * argv[]) {
-    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+int main(){
+    FASTIO;
     
-    cin>>n;
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            cin>>map[i][j];
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> cost[i][j];
         }
     }
     
-    memset(dp, -1, sizeof(dp)); //dp배열 -1로 초기화
-    cout<<dfs(0,1);
+    memset(info, -1, sizeof(info));
+    
+    cout << search(0, 1) << endl;
     
     return 0;
 }
