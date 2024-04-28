28bad8faf59180a30f181e8044d7fb8cc150f7ba
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-28 23:08:33 +0900

    [level 2] Title: 롤케이크 자르기, Time: 501.97 ms, Memory: 51.3 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,26 +1,19 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(topping):
-    topping_l = defaultdict(int)
-    topping_r = defaultdict(int)
-    kind_l = 0
-    kind_r = 0
-    for t in topping:
-        topping_r[t] += 1
-        if topping_r[t] == 1:
-            kind_r += 1
+    topping_l = set()
+    topping_r = Counter(topping)
+    kind_r = len(topping_r)
     
     answer = 0
     for t in topping:
-        topping_l[t] += 1
-        if topping_l[t] == 1:
-            kind_l += 1
+        topping_l.add(t)
         
         topping_r[t] -= 1
         if topping_r[t] == 0:
             kind_r -= 1
         
-        if kind_l == kind_r:
+        if len(topping_l) == kind_r:
             answer += 1
     
     return answer
