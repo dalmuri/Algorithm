e4fd7e7a49a31af935ea8e5ee9667348d3ed3564
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-28 19:13:23 +0900

    [level 3] Title: 아이템 줍기, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 0.04 ms
+메모리: 4.13 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 28일 18:58:13
+2024년 04월 28일 19:13:21
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.cpp
@@ -1,24 +1,29 @@
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 vector<vector<int>> direction = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}}; // 상 우 하 좌
 int n = 102;
 int inf = 2000;
-int dest_x, dest_y;
 vector<vector<int>> plane(n, vector<int>(n, 0)); // 0: 빈 공간, 1: 선 위, 2: 다각형 내부
 vector<vector<int>> dist(n, vector<int>(n, inf));
+queue<vector<int>> q;
 
-void search(int x, int y, int len){
+void search(){
+    int x = q.front()[0];
+    int y = q.front()[1];
+    int len = q.front()[2];
+    q.pop();
+    
     if(x < 0 || x >= n || y < 0 || y >= n) return;
     if(plane[x][y] != 1) return;
     if(dist[x][y] <= len) return;
     
     dist[x][y] = len;
-    if(x == dest_x && y == dest_y) return;
     
     for(vector<int> dir : direction){
-        search(x + dir[0], y + dir[1], len + 1);
+        q.push({x + dir[0], y + dir[1], len + 1});
     }
 }
 
@@ -36,9 +41,14 @@ int solution(vector<vector<int>> rectangle, int characterX, int characterY, int 
         }
     }
     
-    dest_x = itemX * 2;
-    dest_y = itemY * 2;
-    search(characterX * 2, characterY * 2, 0);
+    int dest_x = itemX * 2;
+    int dest_y = itemY * 2;
+    q.push({characterX * 2, characterY * 2, 0});
+    while(!q.empty()){
+        search();
+        
+        if(dist[dest_x][dest_y] < inf) break;
+    }
     
     return dist[dest_x][dest_y] / 2;
 }
