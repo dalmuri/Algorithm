79df75ebcd212bfd73fcdea7038d60d69c5649e3
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-28 21:33:07 +0900

    [Silver I] Title: 회전 초밥, Time: 3436 ms, Memory: 32432 KB -BaekjoonHub

---
+++ 백준/Silver/2531. 회전 초밥/회전 초밥.py
@@ -0,0 +1,17 @@
+import sys
+
+n, d, k, c = map(int, sys.stdin.readline().split())
+sushi = [0] * n
+for i in range(n):
+    sushi[i] = int(sys.stdin.readline())
+sushi += sushi[:k - 1]
+
+answer = 0
+for i in range(len(sushi) - k + 1):
+    kinds = set(sushi[i:i+k])
+    answer = max(answer, len(kinds) if c in kinds else len(kinds) + 1)
+    
+    if answer == d:
+        break
+
+print(answer)
