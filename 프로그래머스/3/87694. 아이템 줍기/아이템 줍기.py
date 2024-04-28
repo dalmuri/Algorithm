7050554ff649c4b6520e81d0daf115f7c13f8692
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-28 19:52:41 +0900

    [level 3] Title: 아이템 줍기, Time: 0.35 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,34 +1,31 @@
-import sys
-sys.setrecursionlimit(10**5)
-inf = 10000
-n = 102
+from itertools import count
 
-plane = [[0] * n for _ in range(n)] # 0: 없음 1: 테두리 2: 사각형 내부
-dist = [[inf] * n for _ in range(n)]
 direction = [[-1, 0], [0, 1], [1, 0], [0, -1]] # 상 우 하 좌
 
-def search(x, y, length, dest_x, dest_y):
-    if x < 0 or x >= n or y < 0 or y >= n: return
-    if plane[x][y] != 1: return
-    if dist[x][y] <= length: return
+def movable(now_x, now_y, next_x, next_y, rectangle):
+    x = (now_x + next_x) / 2
+    y = (now_y + next_y) / 2
+    on_line = any((x1 <= x <= x2 and y in (y1, y2)) or (x in (x1, x2) and y1 <= y <= y2) for x1, y1, x2, y2 in rectangle)
+    in_rectangle = any(x1 < x < x2 and y1 < y < y2 for x1, y1, x2, y2 in rectangle)
     
-    dist[x][y] = length
-    if x == dest_x and y == dest_y: return
-    
-    for d in direction:
-        search(x + d[0], y + d[1], length + 1, dest_x, dest_y)
+    return on_line and not in_rectangle
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
-    for rec in rectangle:
-        for x in range(rec[0] * 2, rec[2] * 2 + 1):
-            for y in range(rec[1] * 2, rec[3] * 2 + 1):
-                if plane[x][y] == 2: continue
-                
-                if x == rec[0] * 2 or x == rec[2] * 2 or y == rec[1] * 2 or y == rec[3] * 2:
-                    plane[x][y] = 1
-                else:
-                    plane[x][y] = 2
+    now = [characterX, characterY]
+    prev = None
     
-    search(characterX * 2, characterY * 2, 0, itemX * 2, itemY * 2)
+    for dist in count(0):
+        if now == [characterX, characterY] and prev != None: # 한 바퀴 회전
+            dist_cycle = dist
+            break
+        elif now == [itemX, itemY]:
+            dist_item = dist
+        
+        for d in direction:
+            nxt = [now[0] + d[0], now[1] + d[1]]
+            if nxt != prev and movable(*now, *nxt, rectangle):
+                prev = now
+                now = nxt
+                break
     
-    return dist[itemX * 2][itemY * 2] // 2
+    return min(dist_item, dist_cycle - dist_item)
