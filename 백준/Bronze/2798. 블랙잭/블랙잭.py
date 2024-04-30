627a8645226a6dce69ffaadeabc9a3b8a2ca6b7c
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-04-30 16:21:19 +0900

    [Bronze II] Title: 블랙잭, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,11 @@
 import sys
-import itertools
+from itertools import combinations
 
 n, m = map(int, sys.stdin.readline().split())
 nums = list(map(int, sys.stdin.readline().split()))
 
 answer = 0
-for a, b, c in itertools.combinations(nums, 3):
+for a, b, c in combinations(nums, 3):
     if answer < a + b + c <= m:
         answer = a + b + c
 
