855f282e834e138e4af0f332b3a451bbf73fa0db
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-05-01 21:35:29 +0900

    [Gold V] Title: 색칠 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1117. 색칠 1/색칠 1.py
+++ 백준/Gold/1117. 색칠 1/색칠 1.py
@@ -4,4 +4,4 @@ w, h, f, c, x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 
 folded = f if f * 2 <= w else w - f
 
-print(w * h - (max(0, (min(x2, folded) - x1) * (y2 - y1) * (c + 1) * 2) + max(0, (x2 - max(x1, folded)) * (y2 - y1) * (c + 1))))
+print(w * h - max(0, (min(x2, folded) - x1) * (y2 - y1) * (c + 1) * 2) - max(0, (x2 - max(x1, folded)) * (y2 - y1) * (c + 1)))
