55fc8d93490116bb22e664428a04b3c00cc8d6e0
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-05-04 19:51:54 +0900

    [Silver III] Title: 계단 오르기, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,17 +1,17 @@
 import sys
 
 n = int(sys.stdin.readline())
-stair = [0]
+point = [0]
 for _ in range(n):
-    stair.append(int(sys.stdin.readline()))
+    point.append(int(sys.stdin.readline()))
 
-point = [[0, 0] for _ in range(n + 1)]
-# point[i][0] = 구간 [1, i]에서 stair[i]를 이전 계단과 연속하지 않고 밟을 때의 점수 최댓값
-# point[i][1] = 구간 [1, i]에서 stair[i]를 이전 계단과 연속해서 밟을 때의 점수 최댓값
-point[1] = [stair[1], stair[1]]
+stair = [[0, 0] for _ in range(n + 1)]
+# stair[i][0] = 구간 [1, i]에서 i계단을 이전 계단과 연속하지 않고 밟을 때의 점수 최댓값
+# stair[i][1] = 구간 [1, i]에서 i계단을 이전 계단과 연속해서 밟을 때의 점수 최댓값
+stair[1] = [point[1], point[1]]
 
 for i in range(2, n + 1):
-    point[i][0] = max(point[i - 2]) + stair[i]
-    point[i][1] = point[i - 1][0] + stair[i]
+    stair[i][0] = max(stair[i - 2]) + point[i]
+    stair[i][1] = stair[i - 1][0] + point[i]
 
-print(max(point[n]))
+print(max(stair[n]))
