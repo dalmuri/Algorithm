66365fe9fb104bfcc6d5a483b8bf89b431755547
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-05-04 19:49:29 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -1,23 +1,17 @@
 import sys
 
 n = int(sys.stdin.readline())
-stair = []
+stair = [0]
 for _ in range(n):
     stair.append(int(sys.stdin.readline()))
 
-dp = [[0, 0] for _ in range(n)]
-# dp[i][0] = stair[i]를 이전 계단과 연속하지 않고 밟을 때의 점수 최댓값
-# dp[i][1] = stair[i]를 이전 계단과 연속해서 밟을 때의 점수 최댓값
-dp[0] = [stair[0], stair[0]]
+point = [[0, 0] for _ in range(n + 1)]
+# point[i][0] = 구간 [1, i]에서 stair[i]를 이전 계단과 연속하지 않고 밟을 때의 점수 최댓값
+# point[i][1] = 구간 [1, i]에서 stair[i]를 이전 계단과 연속해서 밟을 때의 점수 최댓값
+point[1] = [stair[1], stair[1]]
 
-if n == 1:
-    print(stair[0])
-else:
-    dp[1][0] = stair[1]
-    dp[1][1] = stair[0] + stair[1]
-    
-    for i in range(2, n):
-        dp[i][0] = max(dp[i - 2]) + stair[i]
-        dp[i][1] = dp[i - 1][0] + stair[i]
-    
-    print(max(dp[n - 1]))
+for i in range(2, n + 1):
+    point[i][0] = max(point[i - 2]) + stair[i]
+    point[i][1] = point[i - 1][0] + stair[i]
+
+print(max(point[n]))
