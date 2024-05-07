579d714fa143f6894169216339cd139f2e77a04b
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-05-08 00:10:21 +0900

    [Gold V] Title: Fly me to the Alpha Centauri, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
+++ 백준/Gold/1011. Fly me to the Alpha Centauri/Fly me to the Alpha Centauri.cc
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <climits>
 
 #define FASTIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-#define ll long long
 
 using namespace std;
 
@@ -17,20 +15,11 @@ int main() {
         cin >> x >> y;
 
         int dist = y - x;
-        ll left = 0;
-        ll right = INT_MAX; // 2^31 - 1
-        ll n = right;
-        while(left <= right){
-            ll mid = (left + right) / 2;
-
-            if(dist <= mid * mid + mid){
-                n = mid;
-                right = mid - 1;
-            }
-            else left = mid + 1;
+        int answer = 0;
+        for(long long n = 1; answer == 0; n++){
+            if(dist <= n * n) answer = n * 2 - 1;
+            else if(n * n < dist && dist <= n * n + n) answer = n * 2;
         }
-
-        int answer = dist > n * n ? 2 * n : 2 * n - 1;
 
         cout << answer << "\n";
     }
