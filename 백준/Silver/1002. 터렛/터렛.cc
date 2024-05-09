4c32ae9b15bd4da7de5113e379332a97c2023e51
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-05-09 21:30:00 +0900

    [Silver III] Title: 터렛, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.cc
+++ 백준/Silver/1002. 터렛/터렛.cc
@@ -1,11 +1,23 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
 #include <iostream>
 #include <string>
 #include <cmath>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
 
 using namespace std;
 
 int main()
 {
+    FASTIO;
+    
     int t;
     cin >> t;
     
