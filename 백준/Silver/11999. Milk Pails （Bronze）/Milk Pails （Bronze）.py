68022c9623f6b0d168c963591045a25ed3e46306
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-06-23 19:28:26 +0900

    [Silver V] Title: Milk Pails (Bronze), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11999. Milk Pails （Bronze）/Milk Pails （Bronze）.py
+++ 백준/Silver/11999. Milk Pails （Bronze）/Milk Pails （Bronze）.py
@@ -5,15 +5,14 @@ x, y, m = map(int, sys.stdin.readline().split())
 milk = [False] * (m + 1)
 milk[x] = True
 milk[y] = True
-answer = y
+answer = 0
 
 for i in range(x, m + 1):
     if milk[i]:
+        answer = i
         if i + x <= m:
             milk[i + x] = True
-            answer = max(answer, i + x)
         if i + y <= m:
             milk[i + y] = True
-            answer = max(answer, i + y)
 
 print(answer)
