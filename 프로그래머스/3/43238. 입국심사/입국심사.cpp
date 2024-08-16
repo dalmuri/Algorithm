8f22a54149b16826255e39eb0be94a4911849372
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-08-16 22:53:53 +0900

    [level 3] Title: 입국심사, Time: 55.89 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <vector>
 #include <algorithm>
 #include <numeric>
@@ -8,22 +9,16 @@ long long solution(int n, vector<int> times) {
     long long left = 1;
     long long right = (long long)*max_element(times.begin(), times.end()) * n;
     long long answer = right;
-    
     while(left <= right){
         long long mid = (left + right) / 2;
-        
-        long long sum = accumulate(times.begin(), times.end(), 0ll, [&](long long init, int e){
-            return init + mid / e;
-        });
-        
-        if(n <= sum){
-            answer = mid;
+        long long sum = accumulate(times.begin(), times.end(), 0ll, [&](long long acc, long long now){ return acc + mid / now;});
+        if(sum >= n){
             right = mid - 1;
+            answer = mid;
         }
         else{
             left = mid + 1;
         }
     }
-    
     return answer;
 }
