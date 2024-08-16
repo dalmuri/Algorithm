9181b073ac6d5a802c2134800106b2d36f4e9537
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-08-16 22:12:20 +0900

    [level 3] Title: 합승 택시 요금, Time: 11.24 ms, Memory: 9.84 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -3,50 +3,49 @@
 #include <queue>
 
 using namespace std;
-int inf = 60'000'001;
+int inf = 87654321;
 
 struct cmp{
-  bool operator()(pair<int, int> a, pair<int, int> b) {
-      return a.second > b.second;
-  }  
+    bool operator()(pair<int, int> a, pair<int, int> b){
+        return a.second > b.second;
+    }
 };
 
-vector<int> dijkstra(int start, int n, vector<vector<vector<int>>>& cost){
+vector<int> dijkstra(int start, int n, vector<vector<vector<int>>>& roads){
+    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq; // {next, next_dist}
     vector<int> dist(n + 1, inf);
     dist[start] = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq; // idx, distance
-    pq.push(make_pair(start, 0));
+    pq.push({start, 0});
     
     while(!pq.empty()){
-        int now_idx = pq.top().first;
+        int now = pq.top().first;
         int now_dist = pq.top().second;
         pq.pop();
         
-        if(dist[now_idx] < now_dist) continue;
-        
-        for(vector<int> next : cost[now_idx]){
-            int next_idx = next[0];
-            int next_dist = now_dist + next[1];
-            
-            if(next_dist < dist[next_idx]){
-                dist[next_idx] = next_dist;
-                pq.push(make_pair(next_idx, next_dist));
+        if(now_dist > dist[now]) continue;
+        for(vector<int> road : roads[now]){
+            int next = road[0];
+            int next_dist = now_dist + road[1];
+            if(next_dist < dist[next]){
+                dist[next] = next_dist;
+                pq.push({next, next_dist});
             }
         }
     }
+    
     return dist;
 }
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    vector<vector<vector<int>>> cost(n + 1, vector<vector<int>>());
+    vector<vector<vector<int>>> roads(n + 1, vector<vector<int>>());
     for(vector<int> fare : fares){
-        cost[fare[0]].push_back({fare[1], fare[2]});
-        cost[fare[1]].push_back({fare[0], fare[2]});
+        roads[fare[0]].push_back({fare[1], fare[2]});
+        roads[fare[1]].push_back({fare[0], fare[2]});
     }
     
-    vector<int> dist_s = dijkstra(s, n, cost);
-    vector<int> dist_a = dijkstra(a, n, cost);
-    vector<int> dist_b = dijkstra(b, n, cost);
+    vector<int> dist_s = dijkstra(s, n, roads);
+    vector<int> dist_a = dijkstra(a, n, roads);
+    vector<int> dist_b = dijkstra(b, n, roads);
     
     int answer = inf;
     for(int i = 1; i <= n; i++){
