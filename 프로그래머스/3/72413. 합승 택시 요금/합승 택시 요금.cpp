f45677e45870665ebc6d55ac0b7c102bdccd1d14
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-09-09 19:30:40 +0900

    [level 3] Title: 합승 택시 요금, Time: 21.66 ms, Memory: 9.89 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.cpp
@@ -3,7 +3,8 @@
 #include <queue>
 
 using namespace std;
-int inf = 87654321;
+
+int inf = 60'000'001;
 
 struct cmp{
     bool operator()(pair<int, int> a, pair<int, int> b){
@@ -11,45 +12,44 @@ struct cmp{
     }
 };
 
-vector<int> dijkstra(int start, int n, vector<vector<vector<int>>>& roads){
-    priority_queue<pair<int, int>, vector<pair<int, int>>, cmp> pq; // {next, next_dist}
+vector<int> dijkstra(int start, int n, vector<vector<vector<int>>>& costs){
     vector<int> dist(n + 1, inf);
     dist[start] = 0;
+    priority_queue<pair<int, int>> pq;
     pq.push({start, 0});
-    
     while(!pq.empty()){
         int now = pq.top().first;
         int now_dist = pq.top().second;
         pq.pop();
         
         if(now_dist > dist[now]) continue;
-        for(vector<int> road : roads[now]){
-            int next = road[0];
-            int next_dist = now_dist + road[1];
+        for(vector<int> cost : costs[now]){
+            int next = cost[0];
+            int next_dist = now_dist + cost[1];
+            
             if(next_dist < dist[next]){
                 dist[next] = next_dist;
                 pq.push({next, next_dist});
             }
         }
     }
-    
     return dist;
 }
 
 int solution(int n, int s, int a, int b, vector<vector<int>> fares) {
-    vector<vector<vector<int>>> roads(n + 1, vector<vector<int>>());
+    vector<vector<vector<int>>> costs(n + 1, vector<vector<int>>());
     for(vector<int> fare : fares){
-        roads[fare[0]].push_back({fare[1], fare[2]});
-        roads[fare[1]].push_back({fare[0], fare[2]});
+        costs[fare[0]].push_back({fare[1], fare[2]});
+        costs[fare[1]].push_back({fare[0], fare[2]});
     }
     
-    vector<int> dist_s = dijkstra(s, n, roads);
-    vector<int> dist_a = dijkstra(a, n, roads);
-    vector<int> dist_b = dijkstra(b, n, roads);
+    vector<int> from_s = dijkstra(s, n, costs);
+    vector<int> to_a = dijkstra(a, n, costs);
+    vector<int> to_b = dijkstra(b, n, costs);
     
     int answer = inf;
     for(int i = 1; i <= n; i++){
-        answer = min(answer, dist_s[i] + dist_a[i] + dist_b[i]);
+        answer = min(answer, from_s[i] + to_a[i] + to_b[i]);
     }
     return answer;
 }
