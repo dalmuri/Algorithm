69065fe0318eaffc034a1f96a7c464033e999145
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-09-09 19:50:24 +0900

    [level 3] Title: 징검다리 건너기, Time: 2.59 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.cpp
@@ -8,7 +8,7 @@ int solution(vector<int> stones, int k) {
     int answer = INT_MAX;
     deque<int> window; // idx 저장
     for(int i = 0; i < stones.size(); i++){
-        while(!window.empty() && i - window.front() + 1 > k) window.pop_front(); // window 개수가 k개보다 많으면 pop_front
+        while(!window.empty() && i - window.front() > k - 1) window.pop_front(); // window 개수가 k-1개보다 많으면 pop_front
         while(!window.empty() && stones[window.back()] < stones[i]) window.pop_back(); // 현재 stone보다 작은 수를 모두 제거
         
         window.push_back(i);
