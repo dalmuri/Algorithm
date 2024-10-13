5addfb0964904dcfab4c858cba2e16c407cea93d
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-10-14 00:50:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    char c;
+    cin >> c;
+    cout << (int)c;
+
+    return 0;
+}
