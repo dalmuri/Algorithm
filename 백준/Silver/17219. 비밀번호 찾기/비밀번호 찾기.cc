2ff682adeb9455119a55ccd0db273159b7a2e480
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-10-30 01:06:39 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 212 ms, Memory: 16680 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n, m;
+    cin >> n >> m;
+
+    map<string, string> passwords;
+    for(int i = 0; i < n; i++){
+        string website, password;
+        cin >> website >> password;
+
+        passwords[website] = password;
+    }
+
+    for(int i = 0; i < m; i++){
+        string website;
+        cin >> website;
+
+        cout << passwords[website] << "\n";
+    }
+
+    return 0;
+}
