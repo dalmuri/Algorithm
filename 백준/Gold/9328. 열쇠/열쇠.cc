57c394d69b170853211c2f2b463808f7bc34ddd3
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-10 18:27:07 +0900

    [Gold I] Title: 열쇠, Time: 12 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.cc
+++ 백준/Gold/9328. 열쇠/열쇠.cc
@@ -19,7 +19,7 @@ int main()
         vector<string> building(h);
         vector<vector<bool>> visited(h, vector<bool>(w, false));
         vector<bool> has_key(26, false);
-        list<pair<int, int>> doors;
+        vector<queue<pair<int, int>>> doors(26);
         queue<pair<int, int>> q;
         for (int i = 0; i < h; i++){
             cin >> building[i];
@@ -34,20 +34,20 @@ int main()
         for(int i = 0; i < h; i++){
             if(i == 0 || i == h - 1){
                 for(int j = 0; j < w; j++){
-                    if(building[i][j] != '*'){
-                        if('A' <= building[i][j] && building[i][j] <= 'Z' && !has_key[building[i][j] - 'A']) doors.push_back({i, j});
-                        else q.push({i, j});
-                    }
+                    if(building[i][j] == '*') continue;
+
+                    if('A' <= building[i][j] && building[i][j] <= 'Z' && !has_key[building[i][j] - 'A']) doors[building[i][j] - 'A'].push({i, j});
+                    else q.push({i, j});
                 }
             }
             else{
                 if(building[i][0] != '*'){
-                    if ('A' <= building[i][0] && building[i][0] <= 'Z' && !has_key[building[i][0] - 'A']) doors.push_back({i, 0});
+                    if ('A' <= building[i][0] && building[i][0] <= 'Z' && !has_key[building[i][0] - 'A']) doors[building[i][0] - 'A'].push({i, 0});
                     else q.push({i, 0});
                 }
 
                 if(building[i][w - 1] != '*'){
-                    if ('A' <= building[i][w - 1] && building[i][w - 1] <= 'Z' && !has_key[building[i][w - 1] - 'A']) doors.push_back({i, w - 1});
+                    if ('A' <= building[i][w - 1] && building[i][w - 1] <= 'Z' && !has_key[building[i][w - 1] - 'A']) doors[building[i][w - 1] - 'A'].push({i, w - 1});
                     else q.push({i, w - 1});
                 }
             }
@@ -55,43 +55,39 @@ int main()
 
         int answer = 0;
         while(!q.empty()){
-            while(!q.empty()){
-                int r = q.front().first;
-                int c = q.front().second;
-                q.pop();
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
 
-                if(visited[r][c]) continue;
-                visited[r][c] = true;
+            if(visited[r][c]) continue;
+            visited[r][c] = true;
 
-                if('A' <= building[r][c] && building[r][c] <= 'Z' && !has_key[building[r][c] - 'A']) continue;
-                
-                if(building[r][c] == '$') answer++;
-                else if('a' <= building[r][c] && building[r][c] <= 'z') has_key[building[r][c] - 'a'] = true;
-
-                for(vector<int> d : dir){
-                    int next_r = r + d[0];
-                    int next_c = c + d[1];
-                    if(next_r < 0 || next_r >= h || next_c < 0 || next_c >= w) continue;
-                    if(building[next_r][next_c] == '*') continue;
-                    if(visited[next_r][next_c]) continue;
-
-                    if('A' <= building[next_r][next_c] && building[next_r][next_c] <= 'Z'){
-                        if (has_key[building[next_r][next_c] - 'A']) q.push({next_r, next_c});
-                        else doors.push_back({next_r, next_c});
-                    }
-                    else q.push({next_r, next_c});
+            if(building[r][c] == '$') answer++;
+            else if('a' <= building[r][c] && building[r][c] <= 'z'){
+                int idx = building[r][c] - 'a';
+                has_key[idx] = true;
+                while(!doors[idx].empty()){
+                    q.push({doors[idx].front().first, doors[idx].front().second});
+                    doors[idx].pop();
                 }
             }
+            else if('A' <= building[r][c] && building[r][c] <= 'Z' && !has_key[building[r][c] - 'A']){
+                doors[building[r][c] - 'A'].push({r, c});
+                continue;
+            }
 
-            for(auto iter = doors.begin(); iter != doors.end(); iter++){
-                int r = (*iter).first;
-                int c = (*iter).second;
+            for(vector<int> d : dir){
+                int next_r = r + d[0];
+                int next_c = c + d[1];
+                if(next_r < 0 || next_r >= h || next_c < 0 || next_c >= w) continue;
+                if(building[next_r][next_c] == '*') continue;
+                if(visited[next_r][next_c]) continue;
 
-                if(has_key[building[r][c] - 'A']){
-                    q.push({r, c});
-                    auto to_del = iter--;
-                    doors.erase(to_del);
+                if('A' <= building[next_r][next_c] && building[next_r][next_c] <= 'Z'){
+                    if(has_key[building[next_r][next_c] - 'A']) q.push({next_r, next_c});
+                    else doors[building[next_r][next_c] - 'A'].push({next_r, next_c});
                 }
+                else q.push({next_r, next_c});
             }
         }
 
