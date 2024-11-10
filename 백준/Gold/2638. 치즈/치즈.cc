3e50928f0a6b9f749e00999b2688f272a8fa4533
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-10 23:47:13 +0900

    [Gold III] Title: 치즈, Time: 4 ms, Memory: 2276 KB -BaekjoonHub

--- 백준/Gold/2638. 치즈/치즈.cc
+++ 백준/Gold/2638. 치즈/치즈.cc
@@ -6,49 +6,31 @@ using namespace std;
 
 int n, m;
 vector<vector<int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+vector<vector<int>> cheese;
+vector<vector<int>> adj_cnt;
 
-void update_air(int row, int col, vector<vector<int>>& cheese){
+void update_air(int row, int col, queue<pair<int, int>>& q){
     for(vector<int> d : dir){
         int next_row = row + d[0];
         int next_col = col + d[1];
         if(next_row < 0 || next_row >= n || next_col < 0 || next_col >= m) continue;
         if(cheese[next_row][next_col] == 0){
             cheese[next_row][next_col] = -1;
-            update_air(next_row, next_col, cheese);
+            update_air(next_row, next_col, q);
+        }
+        else if(cheese[next_row][next_col] == 1){
+            adj_cnt[next_row][next_col]++;
+            if(adj_cnt[next_row][next_col] == 2) q.push({next_row, next_col});
         }
     }
-}
-
-void melt(int& count, vector<vector<int>>& cheese){
-    vector<vector<int>> next_cheese(cheese);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            if(cheese[i][j] != 1) continue;
-
-            int air_adj = 0;
-            for(vector<int> d : dir){
-                int next_i = i + d[0];
-                int next_j = j + d[1];
-                if(next_i < 0 || next_i >= n || next_j < 0 || next_j >= m) continue;
-                if(cheese[next_i][next_j] == -1) air_adj++;
-            }
-
-            if(air_adj >= 2){
-                next_cheese[i][j] = -1;
-                count--;
-                update_air(i, j, next_cheese);
-            }
-        }
-    }
-
-    cheese = next_cheese;
 }
 
 int main(){
     FASTIO
 
     cin >> n >> m;
-    vector<vector<int>> cheese(n, vector<int>(m)); // 1: 치즈, 0: 내부공기, -1: 외부공기
+    cheese = vector<vector<int>>(n, vector<int>(m)); // 1: 치즈, 0: 내부공기, -1: 외부공기
+    adj_cnt = vector<vector<int>>(n, vector<int>(m, 0));
     int count = 0;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
@@ -57,13 +39,27 @@ int main(){
         }
     }
 
+    queue<pair<int, int>> q;
     cheese[0][0] = -1;
-    update_air(0, 0, cheese);
+    update_air(0, 0, q);
 
     int answer = 0;
     while(count > 0){
         answer++;
-        melt(count, cheese);
+
+        queue<pair<int, int>> to_melt_q;
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            cheese[row][col] = -1;
+            count--;
+
+            update_air(row, col, to_melt_q);
+        }
+        
+        q = to_melt_q;
     }
 
     cout << answer;
