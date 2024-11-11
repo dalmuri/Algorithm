9972b7666d86cf2c3e52a831b1d92df8a5c11103
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-12 00:42:23 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 52 ms, Memory: 32616 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.cc
@@ -15,36 +15,29 @@ int main(){
     FASTIO
 
     int inf = 12345678;
-    int left = 0, right = 0;
-    vector<int> dir = {0};
-    while(true){
-        int d;
-        cin >> d;
-        if(d == 0) break;
-        dir.push_back(d);
-    }
-
-    vector<vector<vector<int>>> dp(dir.size(), vector<vector<int>>(5, vector<int>(5, inf)));
+    vector<vector<vector<int>>> dp(100'002, vector<vector<int>>(5, vector<int>(5, inf)));
     dp[0][0][0] = 0;
 
-    for(int i = 0; i + 1 < dir.size(); i++){
-        int now = dir[i];
-        int next = dir[i + 1];
+    int idx = 0;
+    int now = 0, next;
+    for(; 1; idx++, now = next){
+        cin >> next;
+        if(next == 0) break;
 
         for(int l = 0; l < 5; l++){
-            if(next != now) dp[i + 1][next][now] = min(dp[i + 1][next][now], dp[i][l][now] + calc_power(l, next));
-            if(l != next) dp[i + 1][l][next] = min(dp[i + 1][l][next], dp[i][l][now] + calc_power(now, next));
+            if(next != now) dp[idx + 1][next][now] = min(dp[idx + 1][next][now], dp[idx][l][now] + calc_power(l, next));
+            if(l != next) dp[idx + 1][l][next] = min(dp[idx + 1][l][next], dp[idx][l][now] + calc_power(now, next));
         }
 
         for(int r = 0; r < 5; r++){
-            if(next != now) dp[i + 1][now][next] = min(dp[i + 1][now][next], dp[i][now][r] + calc_power(r, next));
-            if(r != next) dp[i + 1][next][r] = min(dp[i + 1][next][r], dp[i][now][r] + calc_power(now, next));
+            if(next != now) dp[idx + 1][now][next] = min(dp[idx + 1][now][next], dp[idx][now][r] + calc_power(r, next));
+            if(r != next) dp[idx + 1][next][r] = min(dp[idx + 1][next][r], dp[idx][now][r] + calc_power(now, next));
         }
     }
 
     int answer = inf;
-    for(int l = 0; l < 5; l++) answer = min(answer, dp[dir.size() - 1][l][dir.back()]);
-    for(int r = 0; r < 5; r++) answer = min(answer, dp[dir.size() - 1][dir.back()][r]);
+    for(int l = 0; l < 5; l++) answer = min(answer, dp[idx][l][now]);
+    for(int r = 0; r < 5; r++) answer = min(answer, dp[idx][now][r]);
 
     cout << answer;
 
