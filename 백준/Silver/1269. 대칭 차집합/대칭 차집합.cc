54e263edc261368334dddfd3de6f0266942bb5be
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-15 12:06:21 +0900

    [Silver IV] Title: 대칭 차집합, Time: 248 ms, Memory: 20768 KB -BaekjoonHub

---
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n, m;
+    cin >> n >> m;
+
+    map<int, int> cnt;
+    int answer = 0;
+    for(int i = 0; i < n + m; i++){
+        int e;
+        cin >> e;
+
+        cnt[e]++;
+        if(cnt[e] == 1) answer++;
+        else answer--;
+    }
+
+    cout << answer;
+
+    return 0;
+}
