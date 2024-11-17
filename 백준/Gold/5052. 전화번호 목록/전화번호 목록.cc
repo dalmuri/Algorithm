d967b8b2c564e95a3ce4d951c2b5b57e9f3d00c3
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-17 15:52:30 +0900

    [Gold IV] Title: 전화번호 목록, Time: 160 ms, Memory: 136268 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.cc
@@ -5,7 +5,7 @@
 using namespace std;
 
 struct num{
-    bool is_end = false;
+    int is_end = 0; // 0 : 초기 상태, 1 : 마지막 번호, -1: 다음 번호 존재
     vector<num*> next_num;
 
     num(){
@@ -16,13 +16,16 @@ struct num{
         int next = numbers[idx] - '0';
         if(next_num[next] == NULL) next_num[next] = new num();
         
-        if((*next_num[next]).is_end) return false;
+        if((*next_num[next]).is_end == 1) return false;
 
         if(idx + 1 == numbers.size()){
-            (*next_num[next]).is_end = true;
+            if((*next_num[next]).is_end == -1) return false;
+            
+            (*next_num[next]).is_end = 1;
             return true;
         }
 
+        (*next_num[next]).is_end = -1;
         return (*next_num[next]).update(idx + 1, numbers);
     }
 };
@@ -38,19 +41,12 @@ int main(){
 
         num root;
 
-        vector<string> phone_numbers(n);
-        for(int i = 0; i < n; i++){
-            cin >> phone_numbers[i];
-        }
-
-        sort(phone_numbers.begin(), phone_numbers.end());
-
         bool flag = true;
         for(int i = 0; i < n; i++){
-            if(flag && !root.update(0, phone_numbers[i])){
-                flag = false;
-                break;
-            }
+            string numbers;
+            cin >> numbers;
+
+            if(flag && !root.update(0, numbers)) flag = false;
         }
 
         cout << (flag ? "YES\n" : "NO\n");
