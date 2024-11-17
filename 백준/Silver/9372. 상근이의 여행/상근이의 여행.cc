a11b20dfa083fbbdd46d6a90991e97492110d8e2
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-17 17:47:33 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m;
+        cin >> n >> m;
+
+        for(int i = 0; i < m; i++){
+            int a, b;
+            cin >> a >> b;
+        }
+
+        cout << n - 1 << "\n";
+    }
+
+    return 0;
+}
