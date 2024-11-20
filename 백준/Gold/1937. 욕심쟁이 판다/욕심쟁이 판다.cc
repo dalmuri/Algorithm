0cc532a8cad2c760568bb3ceb6c8b14e8b3bfe04
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-20 22:43:17 +0900

    [Gold III] Title: 욕심쟁이 판다, Time: 32 ms, Memory: 4012 KB -BaekjoonHub

--- 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
+++ 백준/Gold/1937. 욕심쟁이 판다/욕심쟁이 판다.cc
@@ -4,47 +4,42 @@
 
 using namespace std;
 
-struct info{
-    int row, col, bamboo;
-
-    bool operator>(const info& other) const{
-        return this->bamboo > other.bamboo;
-    }
-};
 vector<pair<int, int>> dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+int n;
+int forest[501][501];
+int dp[501][501];
+
+int dfs(int r, int c){
+    if(dp[r][c] > 0) return dp[r][c];
+    dp[r][c] = 1;
+
+    for(auto [dr, dc] : dir){
+        int next_r = r + dr;
+        int next_c = c + dc;
+        if(next_r < 0 || next_r >= n || next_c < 0 || next_c >= n) continue;
+        if(forest[r][c] > forest[next_r][next_c]){
+            dp[r][c] = max(dp[r][c], dfs(next_r, next_c) + 1);
+        }
+    }
+
+    return dp[r][c];
+}
 
 int main(){
     FASTIO
 
-    int n;
     cin >> n;
 
-    vector<vector<int>> forest(n, vector<int>(n));
-    priority_queue<info, vector<info>, greater<info>> pq;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             cin >> forest[i][j];
-            pq.push({i, j, forest[i][j]});
         }
     }
 
-    vector<vector<int>> dp(n, vector<int>(n, 1));
-
     int answer = 1;
-    while(!pq.empty()){
-        int r = pq.top().row;
-        int c = pq.top().col;
-        pq.pop();
-
-        for(auto [dr, dc] : dir){
-            int next_r = r + dr;
-            int next_c = c + dc;
-            if(next_r < 0 || next_r >= n || next_c < 0 || next_c >= n) continue;
-            if(forest[next_r][next_c] <= forest[r][c]) continue;
-            if(dp[next_r][next_c] < dp[r][c] + 1){
-                dp[next_r][next_c] = dp[r][c] + 1;
-                answer = max(answer, dp[next_r][next_c]);
-            }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            answer = max(answer, dfs(i, j));
         }
     }
 
