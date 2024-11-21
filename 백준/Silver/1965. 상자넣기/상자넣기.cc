abe43dbe377f2cada0e30e2717531043f16b6658
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-21 13:14:05 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.cc
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -10,14 +10,14 @@ int main(){
     int n;
     cin >> n;
 
-    vector<int> arr(n);
-    for(int i = 0; i < n; i++) cin >> arr[i];
-
     vector<int> lis;
     for(int i = 0; i < n; i++){
-        auto iter = lower_bound(lis.begin(), lis.end(), arr[i]);
-        if(iter == lis.end()) lis.push_back(arr[i]);
-        else (*iter) = arr[i];
+        int num;
+        cin >> num;
+
+        auto iter = lower_bound(lis.begin(), lis.end(), num);
+        if(iter == lis.end()) lis.push_back(num);
+        else (*iter) = num;
     }
 
     cout << lis.size();
