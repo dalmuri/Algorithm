01be4cac021e9e6a0da35cc3b3d6f26bc929673e
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-21 16:18:06 +0900

    [Silver V] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n;
+    cin >> n;
+
+    cout << ((n & 1) ? "CY" : "SK");
+
+    return 0;
+}
