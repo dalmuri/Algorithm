872e5e9aef192f5286150207ee7a55acc2e9be6b
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-21 15:54:35 +0900

    [Gold V] Title: 퇴사 2, Time: 188 ms, Memory: 7880 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.cc
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.cc
@@ -4,22 +4,23 @@
 
 using namespace std;
 
+int dp[1'500'002];
+
 int main(){
     FASTIO
 
     int n;
     cin >> n;
 
-    vector<vector<int>> counsels(n + 1, vector<int>(2));
-    for(int i = 1; i <= n; i++) cin >> counsels[i][0] >> counsels[i][1];
-
-    vector<int> dp(n + 2, 0);
     int answer = 0;
     for(int i = 1; i <= n; i++){
+        int t, p;
+        cin >> t >> p;
+
         dp[i] = max(dp[i], dp[i - 1]);
-        int end_day = i + counsels[i][0];
+        int end_day = i + t;
         if(end_day <= n + 1){
-            dp[end_day] = max(dp[end_day], dp[i] + counsels[i][1]);
+            dp[end_day] = max(dp[end_day], dp[i] + p);
             answer = max(answer, dp[end_day]);
         }
     }
