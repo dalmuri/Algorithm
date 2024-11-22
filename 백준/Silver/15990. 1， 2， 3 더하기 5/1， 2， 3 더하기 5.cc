3d0fb700569fa586da326c07081f61d71247b460
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-22 16:30:59 +0900

    [Silver I] Title: 1, 2, 3 더하기 5, Time: 992 ms, Memory: 3192 KB -BaekjoonHub

---
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int dp[100'001][3];
+
+int main(){
+    FASTIO
+
+    const int mod = 1'000'000'009;
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        memset(dp, 0, sizeof(int[3]) * (n + 1));
+        dp[1][0] = 1;
+        dp[2][1] = 1;
+        dp[3][2] = 1;
+
+        for(int i = 1; i < n; i++){
+            for(int num = 1; num <= 3 && i + num <= n; num++){
+                if(num - 1 != 0) dp[i + num][num - 1] = (dp[i + num][num - 1] + dp[i][0]) % mod;
+                if(num - 1 != 1) dp[i + num][num - 1] = (dp[i + num][num - 1] + dp[i][1]) % mod;
+                if(num - 1 != 2) dp[i + num][num - 1] = (dp[i + num][num - 1] + dp[i][2]) % mod;
+            }
+        }
+
+        cout << accumulate(dp[n], dp[n] + 3, 0, [](int acc, int e){ return (acc + e) % mod; }) << "\n";
+    }
+
+    return 0;
+}
