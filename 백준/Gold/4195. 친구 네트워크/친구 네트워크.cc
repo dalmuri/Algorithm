e1de49fa7d94ff21d4e56b7f8f7b319c591ee47f
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-11-25 16:14:18 +0900

    [Gold II] Title: 친구 네트워크, Time: 96 ms, Memory: 18908 KB -BaekjoonHub

--- 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
+++ 백준/Gold/4195. 친구 네트워크/친구 네트워크.cc
@@ -5,8 +5,8 @@
 using namespace std;
 
 unordered_map<string, int> indexs;
-map<int, int> root;
-map<int, int> network;
+int root[200'001];
+int network[200'001];
 
 int get_index(string& name){
     auto iter = indexs.find(name);
@@ -21,10 +21,8 @@ int get_index(string& name){
 }
 
 int get_root(int idx){
-    auto iter = root.find(idx);
-    if(iter->second == idx) return idx;
-
-    return iter->second = get_root(iter->second);
+    if(root[idx] == idx) return idx;
+    return root[idx] = get_root(root[idx]);
 }
 
 int combine(int a, int b){
@@ -34,16 +32,13 @@ int combine(int a, int b){
     if(root_a == root_b) return network[root_a];
 
     if(root_a > root_b) swap(root_a, root_b);
-
-    auto iter_a = network.find(root_a);
-    auto iter_b = network.find(root_b);
     
-    iter_a->second += iter_b->second;
-    iter_b->second = 0;
+    network[root_a] += network[root_b];
+    network[root_b] = 0;
 
     root[root_b] = root_a;
 
-    return iter_a->second;
+    return network[root_a];
 }
 
 int main(){
@@ -53,8 +48,6 @@ int main(){
     cin >> t;
     while(t--){
         indexs.clear();
-        root.clear();
-        network.clear();
 
         int f;
         cin >> f;
