df2aff88ccefb0b23ff0a2f5878ef59b3cadea71
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-12-01 12:09:44 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.cc
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    vector<long long> dp(10'001, 0ll);
+    dp[0] = 1;
+
+    for(int i = 1; i <= 3; i++){
+        for(int j = 0; i + j <= 10'000; j++){
+            dp[i + j] += dp[j];
+        }
+    }
+
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        cout << dp[n] << "\n";
+    }
+
+    return 0;
+}
