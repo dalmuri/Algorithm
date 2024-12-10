999bc46dbe33eb355848ce3e702d9050806cc94e
dalmuri <132140216+dalmuri@users.noreply.github.com>
2024-12-10 21:22:37 +0900

    [Silver III] Title: 주유소, Time: 12 ms, Memory: 2804 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -16,22 +16,11 @@ int main(){
     for(int i = 0; i < n; i++) cin >> costs[i];
 
     long long answer = 0ll;
-    int dist = 0;
-    int prev_min_cost = 1'000'000'001;
-    for(int i = n - 2; i >= 0; i--){
-        if(prev_min_cost >= costs[i]){
-            prev_min_cost = costs[i];
-            dist += roads[i];
+    int min_cost = costs[0];
+    for(int i = 0; i < n - 1; i++){
+        min_cost = min(min_cost, costs[i]);
 
-            if(i == 0) answer += prev_min_cost * dist;
-            continue;
-        }
-        
-        answer += prev_min_cost * dist;
-        prev_min_cost = costs[i];
-        dist = roads[i];
-
-        if(i == 0) answer += prev_min_cost * dist;
+        answer += min_cost * roads[i];
     }
 
     cout << answer;
