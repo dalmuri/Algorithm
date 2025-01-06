c389b0075fe321672474a8881c99eb62a0e24365
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-01-06 14:37:29 +0900

    [Gold IV] Title: LCS 3, Time: 4 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Gold/1958. LCS 3/LCS 3.cc
+++ 백준/Gold/1958. LCS 3/LCS 3.cc
@@ -9,31 +9,21 @@ int dp[101][101][101];
 int main(){
     FASTIO
 
-    array<string, 3> arr = {"_", "_", "_"};
-    for(int i = 0; i < 3; i++){
-        string str;
-        cin >> str;
+    array<string, 3> arr;
+    for(int i = 0; i < 3; i++) cin >> arr[i];
 
-        arr[i] += str;
-    }
-
-    int answer = 0;
-    for(int i = 1; i < arr[0].size(); i++){
-        for(int j = 1; j < arr[1].size(); j++){
-            for(int k = 1; k < arr[2].size(); k++){
-                if(arr[0][i] == arr[1][j] && arr[0][i] == arr[2][k]){
+    for(int i = 1; i <= arr[0].size(); i++){
+        for(int j = 1; j <= arr[1].size(); j++){
+            for(int k = 1; k <= arr[2].size(); k++){
+                if(arr[0][i - 1] == arr[1][j - 1] && arr[0][i - 1] == arr[2][k - 1])
                     dp[i][j][k] = dp[i - 1][j - 1][k - 1] + 1;
-                }
-                else{
+                else
                     dp[i][j][k] = max({dp[i - 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]});
-                }
-
-                answer = max(answer, dp[i][j][k]);
             }
         }
     }
 
-    cout << answer;
+    cout << dp[arr[0].size()][arr[1].size()][arr[2].size()];
 
     return 0;
 }
