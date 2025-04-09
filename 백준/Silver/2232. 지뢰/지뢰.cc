d601a0341195b140eaebe6644ea65a0e174f3c09
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-04-09 23:06:49 +0900

    [Silver II] Title: 지뢰, Time: 8 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Silver/2232. 지뢰/지뢰.cc
+++ 백준/Silver/2232. 지뢰/지뢰.cc
@@ -14,14 +14,10 @@ bool should_explode(int idx, vector<int>& mines, vector<bool>& exploded){
     return false;
 }
 
-int explode(int idx, vector<int>& mines, vector<bool>& exploded){
-    int cnt = 1;
+void explode(int idx, vector<int>& mines, vector<bool>& exploded){
     exploded[idx] = true;
-
-    for(int i = idx - 1; i >= 0 && !exploded[i] && mines[i] < mines[i + 1]; --i, ++cnt) exploded[i] = true;
-    for(int i = idx + 1; i < mines.size() && !exploded[i] && mines[i] < mines[i - 1]; ++i, ++cnt) exploded[i] = true;
-
-    return cnt;
+    for(int i = idx - 1; i >= 0 && !exploded[i] && mines[i] < mines[i + 1]; --i) exploded[i] = true;
+    for(int i = idx + 1; i < mines.size() && !exploded[i] && mines[i] < mines[i - 1]; ++i) exploded[i] = true;
 }
 
 int main(){
@@ -34,11 +30,10 @@ int main(){
     for(int i = 0; i < n; ++i) cin >> mines[i];
     
     vector<bool> exploded(n, false);
-    int cnt = 0;
     vector<int> answer;
     for(int i = 0; i < n; ++i){
         if(should_explode(i, mines, exploded)){
-            cnt += explode(i, mines, exploded);
+            explode(i, mines, exploded);
             cout << i + 1 << "\n";
         }
     }
