7b6a447a6fd70482e63a55030718c05aaa19991d
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-04-21 22:09:19 +0900

    [Silver III] Title: 대표 자연수, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Silver/2548. 대표 자연수/대표 자연수.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n;
+    cin >> n;
+
+    vector<int> nums(n);
+    for(int i = 0; i < n; ++i) cin >> nums[i];
+
+    sort(nums.begin(), nums.end());
+    
+    cout << nums[(n - 1) / 2];
+
+    return 0;
+}
