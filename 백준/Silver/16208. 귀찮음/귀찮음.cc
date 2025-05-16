516b4f20d0806de6c0813d47f40444648e61dddf
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-05-16 20:26:25 +0900

    [Silver V] Title: 귀찮음, Time: 32 ms, Memory: 3920 KB, Score: 30 point -BaekjoonHub

--- 백준/Silver/16208. 귀찮음/귀찮음.cc
+++ 백준/Silver/16208. 귀찮음/귀찮음.cc
@@ -4,21 +4,6 @@
 
 using namespace std;
 
-void calc(vector<int>& sticks, int size, long long& answer){
-    if(size == 1) return;
-    
-    sort(sticks.begin(), sticks.begin() + size);
-
-    for(int left = 0, right = (size & 1) ? size - 2 : size - 1; left < right; ++left, --right){
-        answer += sticks[left] * sticks[right];
-        sticks[left] += sticks[right];
-    }
-
-    if(size & 1) sticks[size / 2] = sticks[size - 1];
-
-    calc(sticks, (size + 1) / 2, answer);
-}
-
 int main(){
     FASTIO
 
@@ -28,8 +13,12 @@ int main(){
     vector<int> sticks(n);
     for(int i = 0; i < n; ++i) cin >> sticks[i];
 
+    long long sum = accumulate(sticks.begin(), sticks.end(), 0ll);
     long long answer = 0ll;
-    calc(sticks, n, answer);
+    for(int i = 0; i < n; ++i){
+        sum -= sticks[i];
+        answer += sum * sticks[i];
+    }
 
     cout << answer;
 
