c9f79a44fdfebe1382bf91dfb4249615643ede31
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-05-26 20:54:39 +0900

    [Silver IV] Title: 스케이트 연습, Time: 64 ms, Memory: 3976 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -13,7 +13,8 @@ int main(){
     vector<int> speeds(n);
     for(int i = 0; i < n; ++i) cin >> speeds[i];
 
-    int answer = 0, prev = 0;
+    long long answer = 0ll;
+    int prev = 0;
     for(int i = n - 1; i >= 0; --i) answer += prev = min(speeds[i], prev + 1);
     
     cout << answer;
