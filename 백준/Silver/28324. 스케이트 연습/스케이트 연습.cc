c3b698480c902f83ed2b28af3e7b8d4a0925604a
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-05-26 20:50:26 +0900

    [Silver IV] Title: 스케이트 연습, Time: 0 ms, Memory: 2160 KB, Score: 47 point -BaekjoonHub

---
+++ 백준/Silver/28324. 스케이트 연습/스케이트 연습.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n;
+    cin >> n;
+    
+    vector<int> speeds(n);
+    for(int i = 0; i < n; ++i) cin >> speeds[i];
+
+    int answer = 0, prev = 0;
+    for(int i = n - 1; i >= 0; --i) answer += prev = min(speeds[i], prev + 1);
+    
+    cout << answer;
+
+    return 0;
+}
