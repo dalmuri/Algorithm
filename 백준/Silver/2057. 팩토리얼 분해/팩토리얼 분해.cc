5c362f7f91b978da75576b8e0c4a98e52739aafd
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-05-31 17:14:38 +0900

    [Silver V] Title: 팩토리얼 분해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.cc
+++ 백준/Silver/2057. 팩토리얼 분해/팩토리얼 분해.cc
@@ -4,27 +4,25 @@
 
 using namespace std;
 
-long long n;
-vector<long long> fac(1, 1ll);
-bool can_add(long long now, int idx){
-    if(now > n) return false;
-    if(now == n) return true;
-    if(idx < 0) return false;
-    return can_add(now + fac[idx], idx - 1) || can_add(now, idx - 1);
-}
-
 int main(){
     FASTIO
     
+    long long n;
     cin >> n;
+    if(n == 0ll) n = -1ll;
 
+    vector<long long> fac(1, 1ll);
     for(int i = 1; 1; ++i){
         long long next = fac[i - 1] * i;
         if(next > n) break;
         fac.push_back(next);
     }
 
-    cout << (n > 0ll && can_add(0ll, fac.size() - 1) ? "YES" : "NO");
+    for(int i = fac.size() - 1; i >= 0; --i){
+        if(n >= fac[i]) n -= fac[i];
+    }
+
+    cout << (n == 0 ? "YES" : "NO");
 
     return 0;
 }
