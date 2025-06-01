89b9dad75147223d38fed6e2b917b6269a587621
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-06-01 12:46:56 +0900

    [Silver I] Title: 우리집엔 도서관이 있어, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
+++ 백준/Silver/2872. 우리집엔 도서관이 있어/우리집엔 도서관이 있어.cc
@@ -10,13 +10,13 @@ int main(){
     int n;
     cin >> n;
 
-    vector<int> books(n);
-    for(int i = 0; i < n; ++i) cin >> books[i];
-
     int big = 0, answer = 0;
     for(int i = 0; i < n; ++i){
-        if(big > books[i]) answer = max(answer, books[i]);
-        else big = books[i];
+        int book;
+        cin >> book;
+
+        if(big > book) answer = max(answer, book);
+        else big = book;
     }
 
     cout << answer;
