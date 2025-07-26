289a4a055126b80e891d443a7f0c25eb2ac6bbd4
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-07-26 23:38:34 +0900

    [Gold III] Title: 코딩은 예쁘게, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2879. 코딩은 예쁘게/코딩은 예쁘게.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n;
+    cin >> n;
+
+    vector<int> count(n);
+    for(int i = 0; i < n; ++i) cin >> count[i];
+    for(int i = 0, tmp; i < n; ++i){
+        cin >> tmp;
+        count[i] -= tmp;
+    }
+    
+    int answer = 0;
+    for(int i = 0; i < n; ++i){
+        if(count[i] == 0) continue;
+
+        int tab = -count[i];
+        answer += abs(count[i]);
+        for(int j = i + 1; j < n; ++j){
+            if(count[j] == 0 || (count[i] < 0) != (count[j] < 0)) break;
+
+            if(abs(tab) > abs(count[j])) tab = -count[j];
+            count[j] += tab;
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
