966577a08074444ef1d207efe4a3db023c9200a2
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-08-24 10:57:44 +0900

    [Gold IV] Title: 가로등, Time: 272 ms, Memory: 26692 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/32069. 가로등/가로등.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    long long l;
+    int n, k;
+    cin >> l >> n >> k;
+
+    vector<int> lamps(n);
+    for(int i = 0; i < n; ++i) cin >> lamps[i];
+
+    int light = 0;
+    map<int, bool> visited;
+    for(int cnt = 0; cnt < k; ++light){
+        for(int i = 0; i < n && cnt < k; ++i){
+            if(lamps[i] - light >= 0 && !visited[lamps[i] - light]){
+                visited[lamps[i] - light] = true;
+                cout << light << "\n";
+                cnt++;
+            }
+
+            if(lamps[i] + light <= l && !visited[lamps[i] + light] && cnt < k){
+                visited[lamps[i] + light] = true;
+                cout << light << "\n";
+                cnt++;
+            }
+        }
+    }
+
+    return 0;
+}
