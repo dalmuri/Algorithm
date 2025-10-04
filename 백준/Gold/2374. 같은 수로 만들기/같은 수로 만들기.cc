dbed31f1b8a9baa95f402f1054ab94152a512fae
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-10-04 18:35:05 +0900

    [Gold IV] Title: 같은 수로 만들기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.cc
+++ 백준/Gold/2374. 같은 수로 만들기/같은 수로 만들기.cc
@@ -17,16 +17,15 @@ int main(){
     long long answer = 0ll;
     for(int i = 0; i < n; ++i){
         if(!idxs.empty() && idxs.top() <= nums[i]){
-            int now = idxs.top();
             answer += nums[i] - idxs.top();
             while(!idxs.empty() && idxs.top() <= nums[i]) idxs.pop();
         }
         idxs.push(nums[i]);
     }
-    while(idxs.size() > 1){
-        int top = idxs.top();
-        idxs.pop();
-        answer += idxs.top() - top;
+    if(idxs.size() > 1){
+        int low = idxs.top();
+        while(idxs.size() > 1) idxs.pop();
+        answer += idxs.top() - low;
     }
 
     cout << answer;
