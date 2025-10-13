78c84d9a34df4700b875cd88da1bb7a5fbd843bd
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-10-13 22:24:23 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.cc
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.cc
@@ -12,18 +12,17 @@ int main(){
     int n, m;
     cin >> n >> m;
 
-    vector<bool> pages(m + 1, false);
+    vector<bool> pages(n + 1, false);
     for(int i = 0, page; i < m; ++i){
         cin >> page;
         pages[page] = true;
     }
 
-    vector<vector<int>> dp(n + 1, vector<int>(2, inf));
+    vector<vector<int>> dp(n + 1, vector<int>(2, 0));
     // dp[i][0] = i페이지를 인쇄하지 않은 경우 잉크 양. 페이지를 인쇄해야만 하는 경우, inf를 넣음
     // dp[i][1] = i페이지를 인쇄한 경우 잉크 양.
     dp[1][0] = pages[1] ? 0 : inf;
     dp[1][1] = 7;
-
     for(int i = 2; i <= n; ++i){
         dp[i][0] = pages[i] ? min(dp[i - 1][0], dp[i - 1][1]) : inf;
         dp[i][1] = min(dp[i - 1][0] + 7, dp[i - 1][1] + 2);
