5eb5ba95accf254380d3eec47db0c6ff0bca9aa2
dalmuri <132140216+dalmuri@users.noreply.github.com>
2025-10-22 21:57:15 +0900

    [Silver I] Title: 밤양갱, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/31926. 밤양갱/밤양갱.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define FASTIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+    FASTIO
+
+    int n;
+    cin >> n;
+
+    cout << 8 + floor(log2(n)) + 2;
+
+    return 0;
+}
